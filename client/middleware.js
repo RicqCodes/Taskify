56cab6d1361c2c9c910a0e514839e4ecc758ec50
ricqCodes <princenwakanma1996@gmail.com>
2023-04-06 18:01:00 +0100

    Added middleware to protect route

--- client/middleware.js
+++ client/middleware.js
@@ -1,5 +1,3 @@
-import { getCookie, getCookies } from "cookies-next";
-
 import { getEnv } from "./utils/helper";
 
 const defaultHeaders = {
@@ -9,19 +7,16 @@ const defaultHeaders = {
 };
 
 export const middleware = async (req, res) => {
-  const cookie = getCookie("RequestCookies", { req, res });
-  console.log(req.cookies);
-  const resResponse = await fetch(`${getEnv("backendUrl")}/api/getUser`, {
+  const response = await fetch(`${getEnv("backendUrl")}/api/user`, {
     credentials: "include",
     headers: {
       ...defaultHeaders,
       Cookie: req.cookies,
       origin: getEnv("appOrigin"),
-      "XSRF-TOKEN": "",
+      //   "X-XSRF-TOKEN": decodeURI(...req.cookies.getAll()),
     },
   });
+  const data = response.json();
 
-  //   const data = await res.json();
-
-  //   console.log(res);
+  console.log(data);
 };
